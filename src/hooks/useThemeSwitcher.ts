c6e26ca7c4f6bc7e28234b7afdd56e89979c8161
seth-linares <sethlinares1@gmail.com>
2024-08-14 07:30:43 -0400

    Update useThemeSwitcher.ts

--- src/hooks/useThemeSwitcher.ts
+++ src/hooks/useThemeSwitcher.ts
@@ -1,7 +1,7 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-export default function useThemeSwitcher(themes: readonly string[]) {
-    const [currentTheme, setCurrentTheme] = useState<string>(() => {
+export default function useThemeSwitcher<T extends readonly string[]>(themes: T) {
+    const [currentTheme, setCurrentTheme] = useState<T[number]>(() => {
         const savedTheme: string = localStorage.getItem("theme") ?? "";
         // if the theme isn't a part of the possible themes, then set it to dark -> useful for when user incorrectly alters their theme val
         return themes.includes(savedTheme) ? savedTheme : "dark";
@@ -12,14 +12,18 @@ export default function useThemeSwitcher(themes: readonly string[]) {
         // Get theme from our currentTheme state
         // take that saved theme and apply it to our <body> element
         document.body.setAttribute("data-theme", currentTheme);
+        localStorage.setItem("theme", currentTheme);
     }, [currentTheme]);
 
 
-    const changeTheme = (theme: string) => {
-        // store our selected theme into basic local storage for retrieval
-        localStorage.setItem("theme", theme);
+    const changeTheme = (theme: T[number]) => {
         // update our local variable to hold new theme
-        setCurrentTheme(theme);
+        if(themes.includes(theme)) {
+            setCurrentTheme(theme);
+        }
+        else {
+            console.error(`Unknown theme: ${theme}`);
+        }
     };
 
     return {
