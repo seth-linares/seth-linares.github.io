0f104837bd8d85a92ddfeb1941bf2b6390f3d5fe
seth-linares <sethlinares1@gmail.com>
2025-01-27 21:50:40 -0500

    Surely this will fix the scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '../public/vite.svg'
+import viteLogo from './assets/vite.svg'
 import ThemeSwitcher from './components/ThemeSwitcher'
 
 function App() {
