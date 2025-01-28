b4ad8105fd88837073bd5c28afab6fb6bcdbd157
seth-linares <sethlinares1@gmail.com>
2025-01-28 16:38:27 -0500

    new pages

---
+++ src/components/About/AboutPage.tsx
@@ -0,0 +1,19 @@
+// src/components/About/AboutPage.tsx
+
+import React from 'react';
+
+const About: React.FC = () => {
+
+    console.log('About page loaded');
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-4">About Me</h1>
+      <p className="text-base-content/70">
+        Learn more about who I am and what I do.
+      </p>
+    </div>
+  );
+};
+
+export default About;
