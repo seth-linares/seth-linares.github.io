a4d3443dcf45b5853c274a0d8ea0895448177b35
seth-linares <sethlinares1@gmail.com>
2025-01-29 02:10:07 -0500

    fixed path issues with @

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
+import path from 'path';
 
 const ReactCompilerConfig = {
   target: '19' // '17' | '18' | '19'
@@ -9,6 +9,11 @@ const ReactCompilerConfig = {
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     tailwindcss(),
     react({
