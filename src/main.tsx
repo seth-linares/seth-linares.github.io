f5382b8d8043a35dd7ed6b53c3c61ce54c62f14a
seth-linares <sethlinares1@gmail.com>
2025-01-29 08:27:36 -0500

    adding back index.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
 
-const root = createRoot(document.getElementById('root')!)
-
-root.render(
+createRoot(document.getElementById('root')!).render(
   import.meta.env.DEV ? (
     <StrictMode>
       <App />
     </StrictMode>
   ) : (
     <App />
-  )
+  ),
 )
