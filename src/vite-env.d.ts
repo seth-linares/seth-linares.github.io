b3eeb1a0d6d306457ffbada46d2556adfa48429e
seth-linares <sethlinares1@gmail.com>
2025-07-02 13:27:57 -0400

    got rid of env stuff

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,1 @@
 /// <reference types="vite/client" />
-
-
-// This interface defines the shape of environment variables
-interface ImportMetaEnv {
-  readonly VITE_CLAUDE_API_KEY: string  // Declares that VITE_CLAUDE_API_KEY exists and is a string
-  // readonly makes the variable immutable
-  // VITE_ prefix is required for client-side exposure
-}
-
-// This interface augments the global ImportMeta interface
-interface ImportMeta {
-  readonly env: ImportMetaEnv  // Makes your env variables available via import.meta.env
-}
